800ced8c6cc799df829546d0def4e95b313b609d
John Salib <john.salib@teampicnic.com>
2023-03-26 14:41:10 +0200

    First attempt at output

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,67 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { CharStreams, CommonTokenStream } from "antlr4ts";
+import * as vscode from "vscode";
+import { Range } from "vscode";
+import { JSONLexer } from "./antlr/JSONLexer";
+import { JsonContext, JSONParser, ValueContext } from "./antlr/JSONParser";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "template-formatter" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "template-formatter" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "template-formatter.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        "Hello World from Template Formatter!"
+      );
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('template-formatter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Template Formatter!');
-	});
+  vscode.languages.registerDocumentFormattingEditProvider("template", {
+    provideDocumentFormattingEdits(
+      document: vscode.TextDocument
+    ): vscode.TextEdit[] {
+      const text = document.getText();
+      const lexer = new JSONLexer(CharStreams.fromString(text));
+      const tokens = new CommonTokenStream(lexer);
+      const parser = new JSONParser(tokens);
+      const tree = parser.json();
+      const formattedText = tree.toStringTree(parser.ruleNames);
+      const range = document.validateRange(new Range(0, 0, Infinity, Infinity));
+      return [vscode.TextEdit.replace(range, formattedText)];
+    },
+  });
 
-	vscode.languages.registerDocumentFormattingEditProvider('template', {
-		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-			const firstLine = document.lineAt(0);
-			if (firstLine.text !== '42') {
-				return [vscode.TextEdit.insert(firstLine.range.start, '42\n')];
-			} else {
-				return [];
-			}
-		}
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
+// const traverseJson = (json: JsonContext): string =>{
+// 	let output = ""
+	
+// 	for (let index = 0; index < json.childCount; index++) {
+// 		const element = json.getChild(index);
+// 		switch 
+		
+// 	}
+
+// 	return output;
+// };
+
+// const traverseValue = (value: () => ValueContext): string  => {
+
+// }
+
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
