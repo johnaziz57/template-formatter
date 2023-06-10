93eca3729d7dcab80917cd23ea23faeacaa665d1
John Salib <john.salib@teampicnic.com>
2023-06-10 13:18:24 +0200

    formatting if else

--- src/TemplateWalker.ts
+++ src/TemplateWalker.ts
@@ -1,6 +1,6 @@
-import { ParserRuleContext } from "antlr4";
+import { ParserRuleContext, TerminalNode } from "antlr4";
 import JSONListener from "./antlr/JSONListener";
-import {
+import JSONParser, {
     ArrContext,
     JsonContext,
     ObjContext,
@@ -13,12 +13,12 @@ import {
 } from "./antlr/JSONParser";
 
 export class TemplateWalker extends JSONListener {
-    private distance = 0
+    private distance = 0;
     private output = "";
 
     getOutput = (): string => {
         return this.output;
-    }
+    };
 
     enterJson = (ctx: JsonContext) => {
         this.distance = 0;
@@ -32,16 +32,19 @@ export class TemplateWalker extends JSONListener {
     };
     exitObj = (ctx: ObjContext) => {
         this.decreaseDistance();
-        this.output += this.getNewLinePadding()+"}";
+        this.output += this.getNewLinePadding() + "}";
     };
 
     enterObjPair = (ctx: ObjPairContext) => {};
     exitObjPair = (ctx: ObjPairContext) => {};
 
     enterPair = (ctx: PairContext) => {
-        this.output += this.getNewLinePadding() + ctx.STRING().getText() + " : ";
+        this.output +=
+            this.getNewLinePadding() + ctx.STRING().getText() + " : ";
     };
-    exitPair = (ctx: PairContext) => {};
+    exitPair = (ctx: PairContext) => {
+        this.output += ",";
+    };
 
     enterTemplateOperatorPair = (ctx: TemplateOperatorPairContext) => {};
     exitTemplateOperatorPair = (ctx: TemplateOperatorPairContext) => {};
@@ -51,29 +54,45 @@ export class TemplateWalker extends JSONListener {
 
     enterArr = (ctx: ArrContext) => {};
     exitArr = (ctx: ArrContext) => {};
-    
-    enterTemplateOperatorValue = (ctx: TemplateOperatorValueContext) => {};
-    exitTemplateOperatorValue = (ctx: TemplateOperatorValueContext) => {};
+
+    enterTemplateOperatorValue = (ctx: TemplateOperatorValueContext) => {
+        this.output += ctx.START_BLOCK_2().getText();
+        this.increaseDistance();
+    };
+    exitTemplateOperatorValue = (ctx: TemplateOperatorValueContext) => {
+        this.decreaseDistance();
+        this.output += this.getNewLinePadding() + ctx.END_BLOCK_2().getText();
+    };
 
     enterValue = (ctx: ValueContext) => {
-        if (ctx.obj() || ctx.arr()) {return}
+        if (ctx.obj() || ctx.arr()) {
+            return;
+        }
 
         this.output += ctx.getText();
     };
     exitValue = (ctx: ValueContext) => {};
 
+    visitTerminal = (node: TerminalNode): void => {
+        if (node.symbol.type === JSONParser.ELSE_BLOCK_2) {
+            this.decreaseDistance();
+            this.output += this.getNewLinePadding() + node.getText();
+            this.increaseDistance();
+        }
+    }
+
     private getNewLinePadding(): string {
         return "\n" + this.getPadding();
-    }   
+    }
 
     private getPadding(): string {
         return " ".repeat(this.distance);
     }
 
     private increaseDistance() {
-        this.distance += 4
+        this.distance += 4;
     }
     private decreaseDistance() {
-        this.distance -= 4
+        this.distance -= 4;
     }
 }
